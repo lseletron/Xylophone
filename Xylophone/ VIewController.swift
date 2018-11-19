7a1ece833ed5d709808bd7b6502ca81db688269c
Luciano da Silva <luciano.engenharia@soniclear.com.br>
2018-11-18 22:36:03 -0200

    Initial Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,43 @@
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Luciano da Silva on 15/11/2018.
+//  Copyright © 2018 Luciano da Silva. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        playSound()
+        
+    }
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+}
